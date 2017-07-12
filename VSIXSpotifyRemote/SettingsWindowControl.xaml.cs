f122f2c2823bd15b8650846ab4133b48465a0324
Arjan Kuijpers <arjankuijpers@live.nl>
2017-07-12 22:24:00 +0200

    Added Settings Tool Window. first UI draft complete. - TODO: implement functionality, implement saving of settings.

---
+++ VSIXSpotifyRemote/SettingsWindowControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="SettingsWindowControl.xaml.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace VSIXSpotifyRemote
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for SettingsWindowControl.
+    /// </summary>
+    public partial class SettingsWindowControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SettingsWindowControl"/> class.
+        /// </summary>
+        public SettingsWindowControl()
+        {
+            this.InitializeComponent();
+        }
+
+        
+    }
+}
